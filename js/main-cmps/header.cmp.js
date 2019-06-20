99de9c07bd75c956052d147a13e66af773979081
Stav Dorkam <stavforu119@gmail.com>
2019-06-20 13:51:33 +0300

    css styles

--- js/main-cmps/header.cmp.js
+++ js/main-cmps/header.cmp.js
@@ -4,7 +4,10 @@ export default {
     name: 'appHeader',
     template: `
         <header class="app-header">
-            <div class="logo"><img /></div>
+            <div class="logo">
+                <img src="imgs/logo.svg"/>
+                <h1>Appsus</h1>
+            </div>
             <h1>Filter?</h1>
             <label for="header-nav-toggler-label">Input Nav</label>
             <input type="checkbox" id="header-nav-toggler" />
