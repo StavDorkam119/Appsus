9cad5e1c408b959a039501c242995a6761e801bc
Stav Dorkam <stavforu119@gmail.com>
2019-06-20 18:13:16 +0300

    after css

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -75,5 +75,19 @@ export const utilService = {
     getRandomInt,
     makeLorem,
     makeId,
-    colorLog
+    colorLog,
+    timeConverter
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
 }
